d330a27e086387c3ec0fc480563df4df5a14b05c
Arvind Suresh <arvind.suresh60@gmail.com>
2022-08-26 21:47:40 -0400

    Day 1 (#2)

--- main.lisp
+++ main.lisp
@@ -1,9 +1,16 @@
 (load "day-1.lisp")
 
-(defpackage aoc-main
+(defpackage aoc
   (:use :cl
         :aoc-day-1))
-(in-package :aoc-main)
+(in-package :aoc)
 
-(defun main ()
-  (format t "~A~%" (aoc-day-1::part-1)))
+(defun main (day)
+  (let ((input (format nil "inputs/day-~A.txt" day)) results)
+    (cond
+     ((= day 1)
+       (setq results (aoc-day-1::answers input)))
+     (t
+       (error "~A is not a valid day" day)))
+    (format t "Part 1: ~A~%" (car results))
+    (format t "Part 2: ~A~%" (cdr results))))
