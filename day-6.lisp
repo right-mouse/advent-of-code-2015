f1fa9a15cf9e7d67bef6cfa344baafda02f881e2
Arvind Suresh <arvind.suresh60@gmail.com>
2022-10-29 10:20:48 -0400

    Simplify by combining logic

--- day-6.lisp
+++ day-6.lisp
@@ -3,53 +3,27 @@
   (:export :answers))
 (in-package :aoc-day-6)
 
-(defun do-simple-instruction (instruction lights)
+(defun do-instruction (instruction lights simple)
   (let (split-parts
         start-coord end-coord
         x1 x2 y1 y2
         prefix-len
-        new-val
-        is-toggle)
-    (cond
-     ((uiop:string-prefix-p "turn off" instruction)
-       (setq prefix-len 9))
-     ((uiop:string-prefix-p "turn on" instruction)
-       (progn (setq prefix-len 8)
-              (setq new-val t)))
-     ((uiop:string-prefix-p "toggle" instruction)
-       (progn (setq prefix-len 7)
-              (setq is-toggle t)))
-     (t (error "invalid instruction: ~A" instruction)))
-    (setq split-parts (uiop:split-string (subseq instruction prefix-len)))
-    (setq start-coord (uiop:split-string (first split-parts) :separator ","))
-    (setq end-coord (uiop:split-string (third split-parts) :separator ","))
-    (setq x1 (parse-integer (first start-coord)))
-    (setq y1 (parse-integer (second start-coord)))
-    (setq x2 (parse-integer (first end-coord)))
-    (setq y2 (parse-integer (second end-coord)))
-    (loop for i from x1 to x2
-          do (loop for j from y1 to y2
-                   do (setf (aref lights i j)
-                        (if is-toggle
-                            (not (aref lights i j))
-                            new-val))))))
-
-(defun do-elvish-instruction (instruction lights)
-  (let (split-parts
-        start-coord end-coord
-        x1 x2 y1 y2
-        prefix-len
+        new-val is-toggle
         inc-val)
     (cond
      ((uiop:string-prefix-p "turn off" instruction)
        (progn (setq prefix-len 9)
-              (setq inc-val -1)))
+              (unless simple (setq inc-val -1))))
      ((uiop:string-prefix-p "turn on" instruction)
        (progn (setq prefix-len 8)
-              (setq inc-val 1)))
+              (if simple
+                  (setq new-val t)
+                  (setq inc-val 1))))
      ((uiop:string-prefix-p "toggle" instruction)
        (progn (setq prefix-len 7)
-              (setq inc-val 2)))
+              (if simple
+                  (setq is-toggle t)
+                  (setq inc-val 2))))
      (t (error "invalid instruction: ~A" instruction)))
     (setq split-parts (uiop:split-string (subseq instruction prefix-len)))
     (setq start-coord (uiop:split-string (first split-parts) :separator ","))
@@ -60,22 +34,20 @@
     (setq y2 (parse-integer (second end-coord)))
     (loop for i from x1 to x2
           do (loop for j from y1 to y2
-                   do (incf (aref lights i j) (max inc-val (- (aref lights i j))))))))
+                   do (if simple
+                          (setf (aref lights i j)
+                            (if is-toggle (not (aref lights i j)) new-val))
+                          (incf (aref lights i j) (max inc-val (- (aref lights i j)))))))))
 
-(defun count-on (lights)
+(defun count-lights (lights)
   (let ((num 0))
     (destructuring-bind (n m) (array-dimensions lights)
       (loop for i from 0 below n
             do (loop for j from 0 below m
-                     do (when (aref lights i j) (incf num)))))
-    num))
-
-(defun count-brightness (lights)
-  (let ((num 0))
-    (destructuring-bind (n m) (array-dimensions lights)
-      (loop for i from 0 below n
-            do (loop for j from 0 below m
-                     do (incf num (aref lights i j)))))
+                     do (let ((l (aref lights i j)))
+                          (when l (if (eq l t)
+                                      (incf num)
+                                      (incf num l)))))))
     num))
 
 (defun answers (input-file)
@@ -83,6 +55,6 @@
         (binary-lights (make-array '(1000 1000) :element-type 't :initial-element nil))
         (numeric-lights (make-array '(1000 1000))))
     (dolist (instruction input)
-      (do-simple-instruction instruction binary-lights)
-      (do-elvish-instruction instruction numeric-lights))
-    (cons (count-on binary-lights) (count-brightness numeric-lights))))
+      (do-instruction instruction binary-lights t)
+      (do-instruction instruction numeric-lights nil))
+    (cons (count-lights binary-lights) (count-lights numeric-lights))))
